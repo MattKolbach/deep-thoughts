65e00741e59e6af87920475cd9bfa5fe988be216
Matt Kolbach <mattkolbach@yahoo.com>
2022-04-22 10:28:37 -0500

    end 21.3.5

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useQuery } from '@apollo/client'; //this is a hook
+import { QUERY_THOUGHTS } from '../utils/queries';
+import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
+  //use useQuery hook to make query request
+  // eslint-disable-next-line
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || []; //this is optional chaining
+  console.log(thoughts);
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
-      </div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+         {loading ? (
+            <div>Loading...</div>
+         ) : (
+            <ThoughtList thoughts={thoughts} title="Some Food for Thought(s)..." />
+          )}
+       </div>
+     </div>
     </main>
   );
 };
